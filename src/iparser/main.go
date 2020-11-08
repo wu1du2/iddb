3b1e2cbdce7b0ebcec19257c568eaa0110d0108f
wdl <sxwangdalin@ruc.edu.cn>
2020-11-08 17:40:19 +0800

    v0.3

--- src/iparser/main.go
+++ src/iparser/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/marianogappa/sqlparser"
+	"github.com/xwb1989/sqlparser"
 )
 
 func main() {
-	// sql := "select Customer.name ,Orders.quantity,Book.title from Customer,Orders,Book where Customer.id=Orders.customer_id and Book.id=Orders.book_id  and Customer.rank=1 and Book.copies>5000"
-	sql := "SELECT * FROM Customer"
+	sql := "select Customer.name ,Orders.quantity,Book.title from Customer,Orders,Book where Customer.id=Orders.customer_id and Book.id=Orders.book_id  and Customer.rank=1 and Book.copies>5000"
+	// sql := "SELECT * FROM Customer"
 	query, err := sqlparser.Parse(sql)
 	if err != nil {
 		println("ERROR!")
