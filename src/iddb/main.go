05c99e5cb2bfe6f996f9a10bf64877c488106952
wu1du2 <39286932+wu1du2@users.noreply.github.com>
2020-11-19 14:47:40 +0800

    ADD iddb

---
+++ src/iddb/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	// "iparser"
+	// "iqueryanalyzer"
+	// "iqueryoptimizer"
+	// "imeta"
+	// "irpc"
+	// "iexecuter"
+	// "context"
+	// "iexecuter"
+	// "log"
+	// "net"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+}
