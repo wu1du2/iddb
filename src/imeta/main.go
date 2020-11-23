8f9e3f37eb6796458fb862cbebbaf3b88cfc09c0
shuizhiyu <shui_zhiyu@163.com>
2020-11-23 18:44:59 +0800

    update

--- src/imeta/main.go
+++ src/imeta/main.go
@@ -1,59 +1,25 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a client for Greeter service.
 package main
 
 import (
-	"context"
-	"log"
-	"os"
-	"time"
-
-	"google.golang.org/grpc"
-	pb "irpc"
-)
-
-const (
-	// address     = "10.48.66.127:50053"
-	address     = "localhost:50053"
-	defaultName = "wky"
+    "fmt"
+    "github.com/etcd/client/v3"
+    "time"
 )
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewIrpcGreeterClient(conn)
+    /*
+        DialTimeout time.Duration `json:"dial-timeout"`
+        Endpoints []string `json:"endpoints"`
+    */
+    cli, err := clientv3.New(clientv3.Config{
+        Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+        DialTimeout: 5 * time.Second,
+    })
+    if err != nil {
+        fmt.Println("connect failed, err:", err)
+        return
+    }
 
-	// Contact the server and print out its response.
-	name := defaultName
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.IrpcGreet(ctx, &pb.IrpcRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.GetMessage())
+    fmt.Println("connect succ")
+    defer cli.Close()
 }
