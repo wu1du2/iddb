bcc2408073c003c41426e740162a4377d55e092d
wu1du2 <39286932+wu1du2@users.noreply.github.com>
2020-12-03 13:15:52 +0800

    Update main.go

--- src/examples/irpc/irpc_tran_client/main.go
+++ src/examples/irpc/irpc_tran_client/main.go
@@ -12,7 +12,8 @@ type Table struct {
 }
 */
 func main() {
-	address := "localhost:50053"
+	// address := "localhost:50053"
+	address := "10.77.70.161:50053"
 	var table irpctran.Table
 	table.Createstmt = "Create Table PUBLISHER (ID int, NATION varchar(255) );"
 	irpctran.RunTranClient(address, table)
