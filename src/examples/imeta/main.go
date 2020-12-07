0c4ce6e1fca1d3923bb4628f42ff65a2b5f2e6c3
sxwangdalin@ruc.edu.cn <centos@liutongyu-fj-1.novalocal>
2020-12-07 07:15:03 +0000

    update

---
+++ src/examples/imeta/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"imeta"
+	"iplan"
+
+)
+
+
+func main() {
+	imeta.Connect_etcd()
+	err := test_imeta()
+	if( err!=nil ){
+		fmt.Println("err")
+	}
+
+	//imeta.Disconnect_etcd()
+}
+
+func test_imeta()(error){	
+	var tag int64
+	for true{
+		fmt.Println("1:Get_Tree; 2:Get_Node; 3:Set_Node; 4:Build_txn 0: exit")
+		fmt.Scanf("%d",&tag)
+		fmt.Println(tag)
+		if (tag==1) {
+			var nid int64
+			fmt.Println("input txn id")
+			fmt.Scanf("%d",&nid)
+			//var treex iplan.PlanTree
+			treex,err := imeta.Get_Tree(nid)
+			if(err != nil){
+				fmt.Println("err")
+				return err
+			}
+			fmt.Println("--------------")
+			fmt.Println(treex)
+		}
+		if (tag==2) {
+			var tid int64
+			var nid int64
+			fmt.Println("input txn id, node id")
+			fmt.Scanf("%d %d",&tid, &nid)
+			//var nodex iplan.PlanTreeNode
+			nodex,err := imeta.Get_Node(tid, nid)
+			if(err != nil){
+				fmt.Println("err")
+				return err
+			}
+			fmt.Println("--------------")
+			fmt.Println(nodex)
+		}
+		if (tag==3) {
+			var tid int64
+			var nid int64
+			fmt.Println("input txn id, node id")
+			fmt.Scanf("%d %d",&tid, &nid)
+			var nodex iplan.PlanTreeNode
+			nodex.Nodeid = int64(nid)
+			nodex.Left = 2
+			nodex.Right = 3
+			nodex.Parent = 4
+			nodex.Status = 5
+			nodex.TmpTable = "tmp"
+			nodex.Locate = 161
+			nodex.TransferFlag = true
+			nodex.Dest = 161
+			nodex.NodeType = 2
+			nodex.Where = "name=abc"
+			nodex.Cols = "name"
+			nodex.Joint_cols = " "
+			//fmt.Println(nodex)
+			err := imeta.Set_Node(tid, nodex)
+			fmt.Println("--------------")
+			if(err != nil){
+				return err
+			}else{
+				fmt.Println("Put node success")
+			}
+		}
+		if (tag==4){
+			fmt.Println("build txn:")
+			var tid int64
+			fmt.Scanf("%d",&tid)
+			err := imeta.Build_Txn(tid)
+			if(err != nil){
+				return err
+			}
+		}
+		if (tag==0){
+			fmt.Println("bye bye")
+			break
+		}
+	}
+	return nil
+}
+
+
---
+++ src/imeta/imeta.go
@@ -0,0 +1,198 @@
+package imeta
+
+import (
+    "fmt"
+    "github.com/coreos/etcd/clientv3"
+    "time"
+    "context"
+	"strconv"
+	"strings"
+	"iplan"
+)
+
+var Cli *clientv3.Client
+
+func Connect_etcd(){
+   config := clientv3.Config{
+	   Endpoints:   []string{"localhost:2379"},
+	   DialTimeout: 15 * time.Second,
+   }
+   var err error
+   if Cli,err = clientv3.New(config);err!=nil {
+	   fmt.Println(err.Error())
+	   return
+   }	
+   fmt.Println("etcd connect succ")	
+}
+
+// func Disconnect_etcd(){
+// 	Cli.Close
+// }
+
+
+func Build_Txn(txnid int64) (error){
+	kv := clientv3.NewKV(Cli)
+	node0 :=  "/"+strconv.FormatInt(txnid,10)		
+	if putResp, err := kv.Put(context.TODO(),node0,"0",clientv3.WithPrevKV());err !=nil {
+		fmt.Println(err)
+		return err
+	}else{
+		if putResp.PrevKv !=nil {
+			fmt.Println("Not New")
+			fmt.Println(string(putResp.PrevKv.Value))
+		}
+		fmt.Println("Build Txn "+node0+" success")
+		return nil
+	}
+}
+
+func Get_Tree(txnid int64) (iplan.PlanTree, error){
+	kv := clientv3.NewKV(Cli)
+    var treex string
+    treex = strconv.FormatInt(txnid,10)
+    treex = "/"+treex+"/"
+    if getResp, err := kv.Get(context.TODO(),treex,clientv3.WithPrefix());err!=nil {
+		fmt.Println("err1")
+		var a iplan.PlanTree
+		return a,err
+	}else {
+		fmt.Println("Get_Tree:")
+		//fmt.Println(getResp.Kvs)
+		var tree_return iplan.PlanTree
+		var Nnum int64
+
+		node0 :=  "/"+strconv.FormatInt(txnid,10)
+		if getResp, err := kv.Get(context.TODO(),node0);err !=nil {
+			fmt.Println("err3")
+			var a iplan.PlanTree
+			return a,err
+		}else{
+			Nnum,err = strconv.ParseInt(string(getResp.Kvs[0].Value), 10, 64)
+			tree_return.NodeNum = Nnum
+		}
+		for i,mykv :=range getResp.Kvs{
+			if(i > int(Nnum)){
+				break
+			}
+			//fmt.Println(strconv.FormatInt(i,10))
+			//fmt.Println(string(mykv.Key))
+			//fmt.Println(string(mykv.Value))
+
+			s1 := string(mykv.Key)
+			sep := "/"
+			ikeys := strings.Split(s1,sep)
+			// "/txnid/node id"
+			ikey,err := strconv.ParseInt(ikeys[2],10,64)
+			if(err!=nil){
+				fmt.Println("err2")				
+				var b iplan.PlanTree
+				return b,err 
+			}
+
+			//split
+			s2 := string(mykv.Value)
+			sepp := "##"
+			ivalues := strings.Split(s2,sepp)
+			//ivalue -> Node
+			// 1##2##3##4##5##6##7##8##9##10##11##12##13
+			if(len(ivalues)!=13) {
+				fmt.Println("node"+string(mykv.Key)+"has wrong  attribute number:"+strconv.Itoa(len(ivalues)))
+			}
+			//Nodes attr
+			tree_return.Nodes[ikey].Nodeid,err = strconv.ParseInt(ivalues[0],10,64)
+			tree_return.Nodes[ikey].Left,err = strconv.ParseInt(ivalues[1],10,64)
+			tree_return.Nodes[ikey].Right,err = strconv.ParseInt(ivalues[2],10,64)
+			tree_return.Nodes[ikey].Parent,err = strconv.ParseInt(ivalues[3],10,64)
+			tree_return.Nodes[ikey].Status,err = strconv.ParseInt(ivalues[4],10,64)
+			tree_return.Nodes[ikey].TmpTable = ivalues[5]
+			tree_return.Nodes[ikey].Locate,err = strconv.ParseInt(ivalues[6],10,64)
+			tree_return.Nodes[ikey].TransferFlag,err = strconv.ParseBool(ivalues[7])
+			tree_return.Nodes[ikey].Dest,err = strconv.ParseInt(ivalues[8],10,64)
+			tree_return.Nodes[ikey].NodeType,err = strconv.ParseInt(ivalues[9],10,64)
+			tree_return.Nodes[ikey].Where = ivalues[10]
+			tree_return.Nodes[ikey].Cols = ivalues[11]
+			tree_return.Nodes[ikey].Joint_cols = ivalues[12]
+		}
+		return 	tree_return,nil
+	}
+}
+
+func Get_Node(txnid int64, nodeid int64)(iplan.PlanTreeNode, error){
+	kv := clientv3.NewKV(Cli)
+	var nodex string
+	nodex =  "/"+strconv.FormatInt(txnid,10)+"/"+strconv.FormatInt(nodeid,10)
+    if getResp, err := kv.Get(context.TODO(),nodex);err !=nil {
+		var a iplan.PlanTreeNode
+		fmt.Println(err)
+		return a,err
+	}else {
+		//fmt.Println("Get_NODE:")
+		//fmt.Println(getResp.Kvs)
+
+		var Node_return iplan.PlanTreeNode
+
+		//split
+		s2 := string(getResp.Kvs[0].Value)
+		sepp := "##"
+		ivalues := strings.Split(s2,sepp)
+		//ivalue -> Node
+		// 1##2##3##4##5##6##7##8##9##10##11##12##13
+		if(len(ivalues)!=13) {
+			fmt.Println("node"+nodex+"has wrong  attribute number:"+strconv.Itoa(len(ivalues)))
+		}
+			//Nodes attr
+			Node_return.Nodeid,err = strconv.ParseInt(ivalues[0],10,64)
+			Node_return.Left,err = strconv.ParseInt(ivalues[1],10,64)
+			Node_return.Right,err = strconv.ParseInt(ivalues[2],10,64)
+			Node_return.Parent,err = strconv.ParseInt(ivalues[3],10,64)
+			Node_return.Status,err = strconv.ParseInt(ivalues[4],10,64)
+			Node_return.TmpTable = ivalues[5]
+			Node_return.Locate,err = strconv.ParseInt(ivalues[6],10,64)
+			Node_return.TransferFlag,err = strconv.ParseBool(ivalues[7])
+			Node_return.Dest,err = strconv.ParseInt(ivalues[8],10,64)
+			Node_return.NodeType,err = strconv.ParseInt(ivalues[9],10,64)
+			Node_return.Where = ivalues[10]
+			Node_return.Cols = ivalues[11]
+			Node_return.Joint_cols = ivalues[12]
+
+		return Node_return,nil
+	}
+}
+
+func Set_Node(txnid int64, Node_in iplan.PlanTreeNode)(error){
+	kv := clientv3.NewKV(Cli)
+	//key value
+	ikey :=  "/"+strconv.FormatInt(txnid,10)+"/"+strconv.FormatInt(Node_in.Nodeid,10)
+	var ivalue string
+	ivalue =  strconv.FormatInt(Node_in.Nodeid,10)+"##"+strconv.FormatInt(Node_in.Left,10)+"##"+strconv.FormatInt(Node_in.Right,10)+"##"
+	ivalue = ivalue+strconv.FormatInt(Node_in.Parent,10)+"##"+strconv.FormatInt(Node_in.Status,10)+"##"+Node_in.TmpTable+"##"+strconv.FormatInt(Node_in.Locate,10)+"##"
+	ivalue = ivalue+strconv.FormatBool(Node_in.TransferFlag)+"##"+strconv.FormatInt(Node_in.Dest,10)+"##"+strconv.FormatInt(Node_in.NodeType,10)+"##"
+	ivalue = ivalue+Node_in.Where+"##"+Node_in.Cols+"##"+Node_in.Joint_cols
+	
+    if putResp, err := kv.Put(context.TODO(),ikey,ivalue,clientv3.WithPrevKV());err !=nil {
+		fmt.Println(err)
+		return  err
+	}else{
+		if putResp.PrevKv !=nil {
+			fmt.Println("Update:")
+			fmt.Println(string(putResp.PrevKv.Value))
+		}
+		//iplan.PlanTree NodeNum at /txnid
+		node0 :=  "/"+strconv.FormatInt(txnid,10)
+		if getResp, err := kv.Get(context.TODO(),node0);err !=nil {
+			fmt.Println(err)
+			return err
+		}else{
+			var oldnum int64
+			oldnum, err = strconv.ParseInt(string(getResp.Kvs[0].Value), 10, 64)
+			if(Node_in.Nodeid>oldnum){
+				kv.Put(context.TODO(),node0,strconv.FormatInt(Node_in.Nodeid, 10),clientv3.WithPrevKV())
+			}
+			
+		}
+	}
+	return nil
+}
+
+
+
