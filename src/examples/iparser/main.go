8d7ee4e1ceb615e177a2785f9101fb77996b4f8c
wangdalin <sxwangdalin@ruc.edu.cn>
2020-12-15 14:28:48 +0800

    Update main.go

--- src/examples/iparser/main.go
+++ src/examples/iparser/main.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	// sql := "SELECT t1.a,t2.b FROM t1,t2 WHERE t1.a=t2.a and t1.b=t2.b"
-	sql := `select Customer.name,Orders.quantity
-	from Customer,Orders
-	where Customer.id=Orders.customer_id`
+	sql := `select customer.name,orders.quantity
+	from customer,orders
+	where customer.id=orders.customer_id`
 
 	logicalPlanTree := iparser.Parse(sql, 0)
 	// fmt.Println(logicalPlanTree)
