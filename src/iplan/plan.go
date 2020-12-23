1afa37bc41b05c37e47a62d48c127081d3ad7620
sxwangdalin@ruc.edu.cn <centos@liutongyu-fj-1.novalocal>
2020-12-23 11:34:24 +0000

    update imeta interface

--- src/iplan/plan.go
+++ src/iplan/plan.go
@@ -8,46 +8,45 @@ const (
 	MaxFragNum = 10
 )
 
-//PlanTreeNode is the basic node for a PlanTree
 type PlanTreeNode struct {
 	Nodeid       int64
-	Left         int64 //when there is only on child, save in Left
+	Left         int64
 	Right        int64
 	Parent       int64
 	Status       int64  //1 for finished, 0 for waiting
 	TmpTable     string //the name of tmp_table
 	Locate       int64  // the sitenum,n for site_n
-	TransferFlag bool   //true for transer, false for not
+	TransferFlag bool   //1 for transer, 0 for not
 	Dest         int64  //the sitenum of the dest
-	NodeType     int64  //1 for table, 2 for select, 3 for projection, 4 for join, 5 for union
+	NodeType     int64  //1 for table, 2 for select, 3 for projuection, 4 for join, 5 for union
 	//detail string//according to node_type, (1)table_name for table, (2)where_condition for select, (3)col_name for projection, (4)join_type for join, (5)nil for union
 	Where string
+	Rel_cols string //split(",",s)   !!!!
 	Cols  string
 	//cols_type string
+	Joint_type int64 //      !!!!
 	Joint_cols string //"customer_id,id"
 	//union string
 }
-
-//PlanTree saves the tree nodes which are built by iparser
 type PlanTree struct {
 	NodeNum int64
-	Root    int64 //add rootnum to find root
+	Root	int64  //add rootnum to find root !!!
 	Nodes   [MaxNodeNum]PlanTreeNode
 }
 
-type FragNode struct {
-	FragId        int64 //1,2,3,4
-	FragName      string
-	SiteNum       int64 //1,2,3,4
+type FragNode struct{
+	FragId int64  // 
+	FragName      string  //  
 	FragCondition string
-	Ip            string //10.77.10.161
+	SiteNum int64   
+	Ip string
 }
 
-type FragTree struct {
+type FragTree struct{
 	FragNum   int64
-	Frags     [MaxFragNum]FragNode
 	FragType  int64 //0 for Horizontal, 1 for vertical
 	TableName string
+	Frags     [MaxFragNum]FragNode
 }
 
 func (pt *PlanTree) Print() {
