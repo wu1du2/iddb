afd9637810aaabbbbefe777c45d10ab83cd40614
root <root@liutongyu-fj-3.novalocal>
2020-12-24 16:03:46 +0000

    add insert

--- src/examples/iparser/main.go
+++ src/examples/iparser/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"iparser"
 	"iqueryanalyzer"
+	//"github.com/xwb1989/sqlparser"
 )
 
 func main() {
+	//test_old()
+	test_new()
+}
+
+func test_new(){
+
+	//sql := `insert into customer(id,name,rank) values(3000,'Able',1)`
+	sql := `insert into Customer(id, name, rank) values(300001, 'Xiaoming', 1)`
+	//sql := `insert into Customer(id, name, rank) values(300002,'Xiaohong', 1)\
+	//sql := `insert into Publisher(id, name, nation) values(104001,'High Education Press', 'PRC')`
+	//sql := `insert into Book (id, title, authors, publisher_id, copies) values(205001, 'DDB', 'Oszu', 104001, 100)`
+	//sql := `insert into Orders (customer_id, book_id, quantity) values(300001, 205001,5)`
+
+	i,str,j := iparser.HandleInsert(sql)
+	fmt.Println(i)
+	fmt.Println(str)
+	fmt.Println(j)
+
+
+}
+
+
+
+func test_old(){
 	// sql := "SELECT t1.a,t2.b FROM t1,t2"
 
 	sql := `select customer.name,orders.quantity
@@ -18,14 +43,15 @@ func main() {
 	// where customer.id=orders.customer_id
 	// and customer.id=1`
 
-	// sql := `insert into customer(id,name,rank) values(3000,'Able',1)`
+	//sql := `insert into customer(id,name,rank) values(3000,'Able',1)`
 	// iparser.Parse(sql, 0)
 
-	logicalPlanTree := iparser.Parse(sql, 0)
+	logicalPlanTree := iparser.Parse(sql, 0) 
+	//fmt.Print(logicalPlanTree)
 
 	// iqueryanalyzer.ShowPlanTree(logicalPlanTree)
-	physicalPlanTree := iqueryanalyzer.Analyze(logicalPlanTree)
+	physicalPlanTree := iqueryanalyzer.Analyze(logicalPlanTree) 
 
-	iqueryanalyzer.ShowPlanTree(physicalPlanTree)
+	iqueryanalyzer.ShowPlanTree(physicalPlanTree) 
 
 }
